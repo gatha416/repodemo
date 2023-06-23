738357e639b4b109a43aeb31237d4eab4eddd9f5
Training <aish.parida104@gmail.com>
2023-06-23 11:59:22 +0000

    first

---
+++ src/test/java/NewMaven/AppTest.java
@@ -0,0 +1,20 @@
+package NewMaven;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
